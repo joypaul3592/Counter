f2f0ab6efa899e226107c05755840679440e08d1
Joy Paul <joypaul3592@gmail.com>
2022-09-14 19:16:35 +0600

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './Components/Home';
 function App() {
   return (
     <div className="App flex flex-col items-center justify-center bg-gray-400 h-screen">
-      <h1 className=' mb-8 text-5xl font-bold text-gray-500'>Stop Watch</h1>
+      <h1 className=' mb-8 text-3xl font-bold text-gray-500 '>Stop Watch</h1>
       <Home />
     </div>
   );
